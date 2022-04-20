50437ce0c6859ae670d22891e92ae7ffc0a1a824
Isafe-nk <88584761+Isafe-nk@users.noreply.github.com>
2022-04-20 11:01:21 +0800

    Add files via upload

---
+++ conn/connect_db.php
@@ -0,0 +1,16 @@
+<?php
+// Variable to link to database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "library";
+
+
+
+// Conect to database.
+$con = new mysqli($servername,$username,$password,$db);
+if($con->connect_error){
+//If fail.
+die('Connection to database fail:'.$con->connect_error);
+}
+?>
