b668139067843595e6988e4734b1889616daa0a4
Isafe-nk <88584761+Isafe-nk@users.noreply.github.com>
2022-04-24 00:48:11 +0800

    Update deletemember.php

--- deletemember.php
+++ deletemember.php
@@ -10,7 +10,15 @@ include 'conn/connect_db.php';
 $member_id = $_POST['member_id'];
 
 $sql1= " DELETE FROM borrow WHERE member_id='$member_id' ";
-$sql2= " DELETE FROM member WHERE member_id='$member_id'";
+$bullshit = " SELECT borrow_id FROM borrow WHERE member_id = '$member_id' ";
+$abc = mysqli_query($con,$bullshit);
+$bullshit2 = mysqli_fetch_array($abc);
+$abcd = $bullshit2['borrow_id'];
+
+$def = " DELETE FROM borrowdetails WHERE borrow_id='$abcd' ";
+mysqli_query($con, $def);
+
+$sql2= " DELETE FROM member WHERE member_id='$member_id' ";
 $message="Successfully deleted.";
 
 if(isset($_POST ['deletedata'])) {
